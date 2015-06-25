5100b0ffc1bd4fee53e804abb2f6fc25d0854a79
Samuel TRÉGOUËT <samuel.tregouet@gmail.com>
2015-06-25 19:21:45 +0200

    move most server part in `reverse-proxy` package
    and use it

--- app/config.js
+++ app/config.js
@@ -2,6 +2,11 @@
 
 
 module.exports = {
+    proxyHostname: 'debian',
+    proxyPort: '8080',
+    proxyRe: /^\/cw/,
+    dataRe: /^\/data/,
+    publicPath: 'public',
     cw_base_url: 'http://debian:8080/cw'
 };
 
--- server/index.js
+++ server/index.js
@@ -1,66 +1,47 @@
 'use strict';
 
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+var path = require('path'),
+    fs = require('fs');
+var server = require('reverse-proxy'),
+    React = require('react');
 
-var React = require('react');
-
-global.fetch = require('node-fetch');
-global.Promise = require('bluebird');
-global.fetch.Promise = global.Promise;
-
-
-var servePublicFile = require('./file-server');
-var proxyCw = require('./proxy-cw');
-
+var config = require('../app/config');
+config.publicPath = path.join(__dirname, '..', config.publicPath);
 var app = require('../app/app');
+
 
 
 var template = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf8');
 
-
-function onRequest(req, res) {
-    console.log('url', req.url);
-
-    if (/^\/data/.test(req.url)) {
-        console.log('\tdata url => serve file');
-        servePublicFile(req.url.replace(/^\/data/, ''),
-                        res);
-        return;
-    } else if (/^\/cw/.test(req.url)) {
-        console.log('\tcw url => proxy');
-        proxyCw(req, res);
-        return;
-    } else {
-        console.log('\tpage url => react render to string');
-        app.createAppElement(req.url).then(function(appElement) {
-            // write initialdata into html so that on client side
-            // we do not have to ask for the same data
-            var html = template.replace('<!-- init-data -->',
-                                        'var INITIALDATA = ' +
-                                        JSON.stringify(appElement.props.route.data) +
-                                        ';');
-            // write dom computed by react
-            html = html.replace('<!-- ici -->',
-                                React.renderToString(appElement));
-            res.writeHead(200, {
-                'Content-Length': html.length,
-                'Content-Type': 'text/html'
-            });
-            res.end(html);
-        }).catch(function(err) {
-            console.log('oups', err);
-            var text = 'not found';
-            res.writeHead(404, {
-                'Content-Length': text.length,
-                'Content-Type': 'text/plain'
-            });
-            res.end(text);
+config.otherUrlCb = function otherUrlCb(req, res){
+    app.createAppElement(req.url).then(function(appElement) {
+        // write initialdata into html so that on client side
+        // we do not have to ask for the same data
+        var html = template.replace('<!-- init-data -->',
+                                    'var INITIALDATA = ' +
+                                    JSON.stringify(appElement.props.route.data) +
+                                    ';');
+        // write dom computed by react
+        html = html.replace('<!-- ici -->',
+                            React.renderToString(appElement));
+        res.writeHead(200, {
+            'Content-Length': html.length,
+            'Content-Type': 'text/html'
         });
-    }
+        res.end(html);
+    }).catch(function(err) {
+        console.log('oups', err);
+        var text = 'not found';
+        res.writeHead(404, {
+            'Content-Length': text.length,
+            'Content-Type': 'text/plain'
+        });
+        res.end(text);
+    });
+};
 
-}
+server.start(config);
 
-http.createServer(onRequest).listen(3000);
-console.log('listening on 3000');
+    
+
+
